6af16aa14353584be26eefa79d8e5ba50737d1dc
mukandrew <murilloandrew@gmail.com>
2022-04-21 14:40:38 -0300

    Added new locations - Added an asset file with the new locations - Added geoJson.js with a function to generate the object with the locations from fecthed file - Changed the script.js to get the locations from async function, inserting this call into the an async function - Changed the index.html to load the geoJson.js script

--- js/script.js
+++ js/script.js
@@ -10,108 +10,15 @@ function onEachFeature(feature, layer) {
     }
 }
 
-var geojsonFeature = {
-    "type": "FeatureCollection",
-    "features": [{
-            "type": "Feature",
-            "properties": {
-                "name": "Faculdade UniAmérica Descomplica",
-                "popupContent": "Faculdade UniAmérica Descomplica<br><br>Polo Boa Viagem, Recife"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -34.89987909793854,
-                    -8.127340405405453
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Faculdade UniAmérica Descomplica",
-                "popupContent": "Faculdade UniAmérica Descomplica<br><br>Polo Aldeota, Fortaleza"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -38.492378890514374,
-                    -3.7351359207312904
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Faculdade UniAmérica Descomplica",
-                "popupContent": "Faculdade UniAmérica Descomplica<br><br>Sede Barra da Tijuca, Rio de Janeiro"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -43.34820181131363,
-                    -23.000873696075292
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Faculdade UniAmérica Descomplica",
-                "popupContent": "Faculdade UniAmérica Descomplica<br><br>Polo Centro, Rio de Janeiro"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -43.17815646529198,
-                    -22.90307080079857
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Faculdade UniAmérica Descomplica",
-                "popupContent": "Faculdade UniAmérica Descomplica<br><br>Polo Campo Grande, Rio de Janeiro"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -43.56474459171295,
-                    -22.89012959969439
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Faculdade UniAmérica Descomplica",
-                "popupContent": "Faculdade UniAmérica Descomplica<br><br>Polo Madureira, Rio de Janeiro"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -43.33747297525406,
-                    -22.87384725976096
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "name": "Faculdade UniAmérica Descomplica",
-                "popupContent": "Faculdade UniAmérica Descomplica<br><br>Polo São Gonçalo, Rio de Janeiro"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -43.045770674943924,
-                    -22.82380064457385
-                ]
-            }
-        },
-    ]
-}
-L.geoJSON(geojsonFeature, {
-    onEachFeature: onEachFeature
-}).addTo(map);
+(async function launchMap() {
+    await getLocations((locations) => {
+        var geojsonFeature = {
+            "type": "FeatureCollection",
+            "features": locations
+        }
+
+        L.geoJSON(geojsonFeature, {
+            onEachFeature: onEachFeature
+        }).addTo(map);
+    });
+})();
